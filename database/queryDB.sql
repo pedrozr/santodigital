85395f937df284d7958d9fc5cd36c50a4ea65f26
Pedro H <anjospha@gmail.com>
2021-10-08 19:49:48 -0300

    Corrigido erros ao importar

---
+++ database/queryDB.sql
@@ -0,0 +1,21 @@
+create database `techeducation`;
+
+use `techeducation`;
+
+create table `Clientes` (
+  `id` int(9) NOT NULL auto_increment,
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `codigo` varchar(255) NOT NULL,
+  `nome_cliente` varchar(255) NOT NULL,
+  `telefone_cliente` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB;
+
+create table `Usuarios` (
+  `id` int(9) NOT NULL auto_increment,
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `login_usuario` varchar(255) NOT NULL,
+  `nome_usuario` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB;
